caad71d3dafcce8ae45116cbd9c58adc82894c88
Ruan Montelo <ruanmontelo@gmail.com>
2021-03-11 23:37:04 -0300

    Adding new logic for tweeting from the index.js file

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const axios = require('axios').default;
-const getOverwatch2ReleaseStatus = require('./getOverwatch2ReleaseStatus.js')
+const Oauth1Helper = require('./utils/getOAuthHeader.js');
+const getOverwatch2ReleaseStatus = require('./getOverwatch2ReleaseStatus.js');
 
 async function getOverwatch2StatusFromWebDriver() {
     try {
         const releaseStatus = await getOverwatch2ReleaseStatus();
-        console.log(releaseStatus);
+        console.log(`Current release status: ${releaseStatus}`); 
+        
+        // await tweetOverwatch2ReleaseStatus(releaseStatus);
+        await tweetOverwatch2ReleaseStatus("We can't be salty in Overwatch 2 yet.");
     } catch (error) {
-        console.log(`Something unexpected has happened during the execution of the program. Error: ${error}`);
+        console.log(`Something unexpected has happened during the execution of the program. ${error}`);
     }
 }
 
+// TODO: 
+// 1 -> Create an array of catch-phrases for the bot to tweet
+// 2 -> The current catch-phrase will have to need a state of "current phrase", so it does not repeat 
+// in the tweets
+// 3 -> Separate all functions to their own files
+
 async function tweetOverwatch2ReleaseStatus(releaseStatus) {
-    const twitterApiUrl = 'https://api.twitter.com/1.1/statuses/update.json';
-
-    // TODO: the authentication info will be in a "git-ignored" JSON file.
-    axios.post(twitterApiUrl, { status: releaseStatus }, {
-        headers: {
-
-        }
+    const twitterApiUrl = `https://api.twitter.com/1.1/statuses/update.json?status=${releaseStatus}`;
+    await axios.post(twitterApiUrl, {}, {
+        headers: Oauth1Helper.getAuthHeaderForRequest(twitterApiUrl, 'POST')
     });
 }
 
---
+++ utils/getOAuthHeader.js
@@ -0,0 +1,34 @@
+const crypto = require('crypto');
+const oauth1a = require('oauth-1.0a');
+const tokens = require('../configs.json');
+
+
+const CONSUMERKEY = tokens.consumerKey;
+const CONSUMERSECRET = tokens.consumerSecret;
+const TOKENKEY = tokens.accessToken;
+const TOKENSECRET = tokens.tokenSecret;
+
+// TODO: Make this class a function.
+class Oauth1Helper {
+    static getAuthHeaderForRequest(url, method) {
+        const oauth = oauth1a({
+            consumer: { key: CONSUMERKEY, secret: CONSUMERSECRET },
+            signature_method: 'HMAC-SHA1',
+            hash_function(base_string, key) {
+                return crypto
+                    .createHmac('sha1', key)
+                    .update(base_string)
+                    .digest('base64')
+            },
+        })
+
+        const authorization = oauth.authorize({ url, method }, {
+            key: TOKENKEY,
+            secret: TOKENSECRET,
+        });
+
+        return oauth.toHeader(authorization);
+    }
+}
+
+module.exports = Oauth1Helper;
